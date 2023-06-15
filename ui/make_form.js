38ac1b313847bf46d6f965a2511aa999d58fe68f
Torbjörn Rathsman <milasudril@gmail.com>
2023-06-15 19:11:18 +0200

    Create array of fields

--- ui/make_form.js
+++ ui/make_form.js
@@ -2,10 +2,16 @@
 
 function generate_form(element_factory, output_element, record_description, types)
 {
+	let fields = new Array();
 	for(let item in record_description)
 	{
+		fields.push({"first": item, "second": record_description[item]})
+	}
+
+	for(let item in fields)
+	{
 		let container = element_factory.createElement("p");
-		let container_content = element_factory.createTextNode(item);
+		let container_content = element_factory.createTextNode(fields[item].first);
 		container.appendChild(container_content);
 		output_element.appendChild(container);
 	}

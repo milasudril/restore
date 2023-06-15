7b0eef8373a744f4763957d2597c2d7a18fed93a
Torbjörn Rathsman <milasudril@gmail.com>
2023-06-15 19:45:38 +0200

    Restructure schema

--- ui/make_form.js
+++ ui/make_form.js
@@ -1,13 +1,44 @@
 "use strict";
 
+/*
+let strcmp = new Intl.Collator(undefined, {numeric:true, sensitivity: "base"}).compare;
+
+function is_short(type_name, type_params)
+{
+	return 1;
+}
+
+function is_short(type)
+{
+	type_parts = type.split(":");
+	if(type_parts[0] === "restore")
+	{ return is_short(type_parts[1], type.type_parameters); }
+
+	// A user-defined type is considered "long" for now
+	return 0;
+}
+*/
+
 function generate_form(element_factory, output_element, record_description, types)
 {
 	let fields = new Array();
 	for(let item in record_description)
-	{
-		fields.push({"first": item, "second": record_description[item]})
-	}
+	{ fields.push({"first": item, "second": record_description[item]}); }
+/*
+	fields.sort(function(a, b){
+		let a_is_short = is_short(a.second.type, a.second.type_parameters);
+		let b_is_short = is_short(b.second.type, b.second.type_parameters);
 
+		if(a_is_short < b_is_short)
+		{ return -1; }
+
+		if(a_is_short > b_is_short)
+		{ return 1; }
+
+		if(a_is_short === b_is_short)
+		{ return strcmp(b.first, a.first); }
+	});
+*/
 	for(let item in fields)
 	{
 		let container = element_factory.createElement("p");

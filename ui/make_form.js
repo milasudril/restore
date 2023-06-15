fb2673bbb130dd356a52a4faea2f9825efb2630e
Torbjörn Rathsman <milasudril@gmail.com>
2023-06-15 20:08:24 +0200

    Sort items such that short items are first, then by name

--- ui/make_form.js
+++ ui/make_form.js
@@ -1,33 +1,35 @@
 "use strict";
 
-/*
 let strcmp = new Intl.Collator(undefined, {numeric:true, sensitivity: "base"}).compare;
 
-function is_short(type_name, type_params)
+function type_is_long(type)
 {
+	let name_split = type.id.split(":");
+	console.log(name_split[0])
+
+	if(name_split[0] === "restore")
+	{
+		if(type.parameters.layout)
+		{
+			if(type.parameters.layout === "long")
+			{ return 1;}
+		}
+		return 0;
+	}
+
+	// For now custom types are always considered as long
 	return 1;
 }
-
-function is_short(type)
-{
-	type_parts = type.split(":");
-	if(type_parts[0] === "restore")
-	{ return is_short(type_parts[1], type.type_parameters); }
-
-	// A user-defined type is considered "long" for now
-	return 0;
-}
-*/
 
 function generate_form(element_factory, output_element, record_description, types)
 {
 	let fields = new Array();
 	for(let item in record_description)
 	{ fields.push({"first": item, "second": record_description[item]}); }
-/*
+
 	fields.sort(function(a, b){
-		let a_is_short = is_short(a.second.type, a.second.type_parameters);
-		let b_is_short = is_short(b.second.type, b.second.type_parameters);
+		let a_is_short = type_is_long(a.second.type);
+		let b_is_short = type_is_long(b.second.type);
 
 		if(a_is_short < b_is_short)
 		{ return -1; }
@@ -36,13 +38,13 @@ function generate_form(element_factory, output_element, record_description, type
 		{ return 1; }
 
 		if(a_is_short === b_is_short)
-		{ return strcmp(b.first, a.first); }
+		{ return strcmp(a.first, b.first); }
 	});
-*/
+
 	for(let item in fields)
 	{
 		let container = element_factory.createElement("p");
-		let container_content = element_factory.createTextNode(fields[item].first);
+		let container_content = element_factory.createTextNode(type_is_long(fields[item].second.type) + " " + fields[item].first);
 		container.appendChild(container_content);
 		output_element.appendChild(container);
 	}

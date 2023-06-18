e53ebd8be0bcbbb452679c0829b967a07d967c9a
Torbjörn Rathsman <milasudril@gmail.com>
2023-06-18 14:53:00 +0200

    Write web site to a wad64

--- make_resource_archive.py
+++ make_resource_archive.py
@@ -7,12 +7,25 @@
 
 import sys
 import json
+import os
+import wad64
+
+def strip_prefix(path):
+	# FIXME: This only works if source_dir == '.'
+	return path[2:]
+
 
 def compile(params):
+	source_dir = params['build_info']['source_dir']
 	output_file_name = params['targets'][0]
-	print(output_file_name, file=sys.stderr)
-	with open(output_file_name, 'w'):
-		pass
+	files_to_add = []
+	for parent_path, _, filenames in os.walk(source_dir + '/ui'):
+		for f in filenames:
+			files_to_add.append(os.path.join(parent_path, f))
+
+	with wad64.Archive(output_file_name, 'rw', 'co') as archive:
+		for file in files_to_add:
+			archive.insert_file('cot', file, strip_prefix(file))
 
 def main(argv):
 	if sys.argv[1] == 'compile':

71860e8fddce067594bdd2b4c403b5751cc63bc6
Torbjörn Rathsman <milasudril@gmail.com>
2023-06-19 16:23:27 +0200

    Add function to read json data from file archive

--- server/json_loaders.cpp
+++ server/json_loaders.cpp
@@ -16,22 +16,44 @@ jopp::container restore::json::load(west::io::fd_ref fd)
 		if(bytes_read == 0)
 		{
 			if(status != jopp::parser_error_code::completed)
-			{ throw std::runtime_error{std::string{"Failed to parse config file: "} + to_string(status)}; }
+			{ throw std::runtime_error{std::string{"Failed to parse JSON file: "} + to_string(status)}; }
 		}
 
 		// Not handling async I/O here
 		if(bytes_read == -1)
-		{ throw west::system_error{"Failed to read config file: ", errno}; }
+		{ throw west::system_error{"Failed to JSON file: ", errno}; }
 
 		auto const res = parser.parse(std::span{std::data(buffer), static_cast<size_t>(bytes_read)});
 		if(res.ec == jopp::parser_error_code::completed)
-		{ break; }
+		{ return root; }
 		else
 		if(res.ec != jopp::parser_error_code::more_data_needed)
-		{ throw std::runtime_error{std::string{"Failed to parse config file: "} + to_string(res.ec)}; }
+		{ throw std::runtime_error{std::string{"Failed to parse JSON file: "} + to_string(res.ec)}; }
 
 		status = res.ec;
 	}
+}
 
-	return root;
+jopp::container restore::json::load(Wad64::InputFile& src)
+{
+	jopp::container root;
+	jopp::parser parser{root};
+	std::array<char, 65536> buffer{};
+	auto status = jopp::parser_error_code::completed;
+	while(true)
+	{
+		auto const bytes_read = src.read(std::as_writable_bytes(std::span{std::data(buffer), std::size(buffer)}));
+		if(bytes_read == 0)
+		{
+			if(status != jopp::parser_error_code::completed)
+			{ throw std::runtime_error{std::string{"Failed to parse JSON file: "} + to_string(status)}; }
+		}
+
+		auto const res = parser.parse(std::span{std::data(buffer), static_cast<size_t>(bytes_read)});
+		if(res.ec == jopp::parser_error_code::completed)
+		{ return root; }
+
+		if(bytes_read != std::size(buffer))
+		{ throw std::runtime_error{std::string{"Failed to parse JSON file: "} + to_string(status)}; }
+	}
 }
--- server/json_loaders.hpp
+++ server/json_loaders.hpp
@@ -3,6 +3,8 @@
 #ifndef RESTORE_JSON_LOADERS_HPP
 #define RESTORE_JSON_LOADERS_HPP
 
+#include "./resource_file.hpp"
+
 #include <jopp/types.hpp>
 #include <west/io_fd.hpp>
 
@@ -14,6 +16,14 @@ namespace restore::json
 	{
 		auto src = west::io::open(filename, O_RDONLY, 0);
 		return load(src.get());
+	}
+
+	jopp::container load(Wad64::InputFile& src);
+
+	inline jopp::container load(resource_file const& archive, char const* filename)
+	{
+		auto src = archive.get_resource(filename);
+		return load(src);
 	}
 
 

0f7f6e937ed0d769db0ff00d29dcd4fa49ce506b
Torbjörn Rathsman <milasudril@gmail.com>
2023-06-19 16:03:59 +0200

    Refactor config loader

--- server/config.hpp
+++ server/config.hpp
@@ -1,15 +1,22 @@
-//@	{"dependencies_extra":[{"ref":"./config.o", "rel":"implementation"}]}
-
 #ifndef RESTORE_CONFIG_HPP
 #define RESTORE_CONFIG_HPP
 
 #include "./json_loaders.hpp"
 
 #include <jopp/types.hpp>
+#include <stdexcept>
 
 namespace restore::config
 {
-	jopp::object load(char const* filename);
+	inline jopp::object load(char const* filename)
+	{
+		auto obj = json::load(filename);
+
+		if(auto ret = obj.get_if<jopp::object>(); ret != nullptr)
+		{ return std::move(*ret); }
+
+		throw std::runtime_error{"Root element in config file is not an object"};
+	}
 }
 
 #endif

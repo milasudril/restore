80a63dc3459048448ac9e1667046fb17fd696566
Torbjörn Rathsman <milasudril@gmail.com>
2023-06-19 10:54:36 +0200

    Begin to take care of error messages

--- server/http_service.hpp
+++ server/http_service.hpp
@@ -4,6 +4,29 @@
 #include <west/http_message_header.hpp>
 #include <west/http_request_handler.hpp>
 
+template<>
+struct jopp::object_converter<west::http::status>
+{
+	static auto to_json(west::http::status status)
+	{
+		return static_cast<jopp::number>(static_cast<std::underlying_type_t<west::http::status>>(status));
+	}
+};
+
+template<>
+struct jopp::object_converter<west::http::finalize_state_result>
+{
+	static auto to_json(west::http::finalize_state_result const& obj)
+	{
+		jopp::object ret;
+		ret.insert("http_status", jopp::to_json(obj.http_status));
+		if(obj.error_message != nullptr)
+		{ ret.insert("error_message", obj.error_message.get()); }
+
+		return ret;
+	}
+};
+
 namespace restore
 {
 	enum class http_req_processing_result{};
@@ -45,8 +68,12 @@ namespace restore
 				header.request_line.request_target.value().data());
 
 			west::http::finalize_state_result validation_result;
-			validation_result.http_status = west::http::status::ok;
-			validation_result.error_message = nullptr;
+			validation_result.http_status = west::http::status::not_found;
+
+			std::string errmsg{"Unknown resource `"};
+			errmsg.append(header.request_line.request_target.value())
+				.append("`");
+			validation_result.error_message = west::make_unique_cstr(errmsg.c_str());
 
 			return validation_result;
 		}
@@ -61,6 +88,8 @@ namespace restore
 
 		auto finalize_state(west::http::field_map& fields)
 		{
+			puts("Finalize state ok");
+
 			fields.append("Content-Length", std::to_string(0))
 				.append("Content-Type", "text/plain");
 
@@ -70,8 +99,10 @@ namespace restore
 			return validation_result;
 		}
 
-		void finalize_state(west::http::field_map&, west::http::finalize_state_result&& fields)
+		void finalize_state(west::http::field_map& fields, west::http::finalize_state_result const& res)
 		{
+			puts("Finalize state failed");
+			auto const errmsg = jopp::to_json(res);
 			fields.append("Content-Length", std::to_string(0))
 				.append("Content-Type", "text/plain");
 		}
@@ -85,6 +116,7 @@ namespace restore
 		}
 	private:
 		std::reference_wrapper<ResourceFile const> m_res_file;
+		std::string m_err_msg;
 	};
 }
 

291d827210d19da7e8eda25ba4aba078df283571
Torbjörn Rathsman <milasudril@gmail.com>
2023-06-21 14:11:32 +0200

    Refactor mainpage

--- ui/utils.js
+++ ui/utils.js
@@ -7,3 +7,17 @@ function find_if(array, predicate)
 	let ret = array.findIndex(predicate);
 	return ret === - 1? array.length : ret;
 }
+
+function fetch_data(url)
+{
+	return fetch("/tasks", {
+		headers: {
+			"Accept": "application/json"
+		}
+	}).then(function(res) {
+		return {succeeded: res.ok, pending_message: res.json()};
+	}).then(async function(data){
+		let message = await data.pending_message;
+		return {succeeded: data.succeeded, message: message};
+	});
+}

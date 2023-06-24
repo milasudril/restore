e740827d8c9aee0d5e61fb54d17a53b16791520e
Torbjörn Rathsman <mialsudril@gmail.com>
2023-06-24 09:14:27 +0200

    Restrict endpoints task_parameters and parameter_types to method GET

--- server/http_service.cpp
+++ server/http_service.cpp
@@ -2,6 +2,33 @@
 
 #include "./http_service.hpp"
 
+namespace
+{
+	auto serve_resource(west::http::request_header const& header, jopp::json_buffer_view content)
+	{
+		if(header.request_line.method == "GET")
+		{
+			return std::pair{
+				west::http::finalize_state_result {
+					.http_status = west::http::status::ok,
+					.error_message = nullptr,
+				},
+				std::optional{restore::cached_json_response_server{content}}
+			};
+		}
+		return std::pair{west::http::finalize_state_result{
+			.http_status = west::http::status::method_not_allowed,
+			.error_message = west::make_unique_cstr("Endpoint only supports method GET"),
+		}, std::optional<restore::cached_json_response_server>{}};
+	}
+	
+	auto serve_task_parameters(west::http::request_header const& header, jopp::json_buffer_view content)
+	{ return serve_resource(header, content); }
+	
+	auto serve_parameter_types(west::http::request_header const& header, jopp::json_buffer_view content)
+	{ return serve_resource(header, content); }
+}
+
 west::http::finalize_state_result restore::http_service::finalize_state(west::http::request_header const& header)
 {
 	auto const& req_method = header.request_line.method;
@@ -10,23 +37,22 @@ west::http::finalize_state_result restore::http_service::finalize_state(west::ht
 	printf("%s %s\n", req_method.value().data(),
 		req_target.value().data());
 
-
 	if(req_target == "/task_parameters")
 	{
-		m_current_server = cached_json_response_server{m_task_params};
-		return west::http::finalize_state_result{
-			.http_status = west::http::status::ok,
-			.error_message = nullptr
-		};
+		auto [retval, server] = serve_task_parameters(header, m_task_params);
+		if(server.has_value())
+		{ m_current_server = std::move(*server); }
+		
+		return retval;
 	}
 
 	if(req_target == "/parameter_types")
 	{
-		m_current_server = cached_json_response_server{m_param_types};
-		return west::http::finalize_state_result{
-			.http_status = west::http::status::ok,
-			.error_message = nullptr
-		};
+		auto [retval, server] = serve_parameter_types(header, m_param_types);
+		if(server.has_value())
+		{ m_current_server = std::move(*server); }
+		
+		return retval;
 	}
 
 	if(req_target == "/tasks")

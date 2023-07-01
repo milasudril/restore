c2c4ae85f0b86c00846ffc5f039837f0605d0113
Torbjörn Rathsman <milasudril@gmail.com>
2023-07-01 20:56:53 +0200

    Refactor server socket config

--- server/server_socket.hpp
+++ server/server_socket.hpp
@@ -12,16 +12,14 @@ namespace restore
 		int max;
 	};
 
-	auto get_server_port_range(jopp::object const& socket_params)
+	inline auto to_inet_port_range(jopp::object const& port_range)
 	{
-		auto const& range = socket_params.get_field_as<jopp::object>("server_port_range");
-
-		auto const min_value = range.get_field_as<jopp::number>("min");
+		auto const min_value = port_range.get_field_as<jopp::number>("min");
 		if(min_value < 0.0 || min_value > 65535.0)
 		{ throw std::runtime_error{"Server port min value is out of range"}; }
 		auto const port_min = static_cast<int>(min_value);
 
-		auto const max_value = range.get_field_as<jopp::number>("max");
+		auto const max_value = port_range.get_field_as<jopp::number>("max");
 		if(max_value < 0.0 || max_value > 65535.0)
 		{ throw std::runtime_error{"Server port min value is out of range"}; }
 		auto const port_max = static_cast<int>(max_value);
@@ -32,22 +30,34 @@ namespace restore
 		return inet_port_range{port_min, port_max};
 	}
 
-	auto create_inet_socket(jopp::object const& socket_params)
+	struct inet_server_socket_params
 	{
-		auto const& remote_address = socket_params.get_field_as<jopp::string>("remote_address");
-		auto const port_range = get_server_port_range(socket_params);
+		west::io::inet_address remote_address;
+		inet_port_range server_port_range;
+	};
+
+	inline auto to_inet_server_socket_params(jopp::object const& socket_params)
+	{
+		return inet_server_socket_params{
+			.remote_address = west::io::inet_address{socket_params.get_field_as<jopp::string>("remote_address").c_str()},
+			.server_port_range = to_inet_port_range(socket_params.get_field_as<jopp::object>("server_port_range"))
+		};
+	}
+
+	inline auto create_inet_socket(inet_server_socket_params const& params)
+	{
 		return west::io::inet_server_socket{
-			west::io::inet_address{remote_address.c_str()},
-			std::ranges::iota_view{port_range.min, port_range.max + 1},
+			params.remote_address,
+			std::ranges::iota_view{params.server_port_range.min, params.server_port_range.max + 1},
 			128
 		};
 	}
 
-	auto create_server_socket(jopp::object const& socket_config)
+	inline auto create_server_socket(jopp::object const& socket_config)
 	{
 		auto const& socket_type = socket_config.get_field_as<jopp::string>("type");
 		if(socket_type == "inet")
-		{ return create_inet_socket(socket_config.get_field_as<jopp::object>("params")); }
+		{ return create_inet_socket(to_inet_server_socket_params(socket_config.get_field_as<jopp::object>("params"))); }
 		else
 		{ throw std::runtime_error{"Unsupported socket type"}; }
 	}

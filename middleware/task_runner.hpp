c64514c97138821e04f76450e8cc2e3fe49f5fe0
Torbjörn Rathsman <milasudril@gmail.com>
2023-07-03 09:12:19 +0200

    More work on task_runner

---
+++ middleware/task_runner.hpp
@@ -0,0 +1,87 @@
+#ifndef RESTORE_TASK_HPP
+#define RESTORE_TASK_HPP
+
+#include "./jopp_wrapper.hpp"
+
+#include <atomic>
+#include <mutex>
+#include <thread>
+
+namespace restore
+{
+	template<class Task>
+	class task_runner
+	{
+	public:
+		explicit task_runner(Task&& task):
+			m_should_stop{false},
+			m_task{std::move(task)}
+		{}
+
+		void suspend()
+		{
+			if(m_runner.joinable())
+			{
+				m_should_stop = true;
+				m_runner.join();
+			}
+		}
+
+		void resume()
+		{
+			suspend();
+			m_runner = std::thread{[this](){ do_run(); }};
+		}
+
+		double get_progress() const
+		{
+			std::lock_guard lock{m_task_mtx};
+			return m_task.get_progress();
+		}
+
+
+		void set_parameters(json::object_ref params)
+		{
+			std::lock_guard lock{m_task_mtx};
+			m_task.set_parameters(params);
+		}
+
+		void suspend_and_dump_state(state_sink sink)
+		{
+			suspend();
+			dump_state(sink);
+		}
+
+		void set_state(state_source src)
+		{
+			std::lock_guard lock{m_task_mtx};
+			m_task.set_state(src);
+		}
+
+		void dump_state(state_sink sink) const
+		{
+			std::lock_guard lock{m_task_mtx};
+			m_task.dump_state(sink);
+		}
+
+		~task_wrapper()
+		{ suspend(); }
+
+	private:
+		std::atomic<bool> m_should_stop;
+		Task m_task;
+		mutable std::mutex m_task_mtx;
+		std::thread m_runner;
+
+		void do_run()
+		{
+			while(!m_should_stop.load())
+			{
+				std::lock_guard lock{m_task_mtx};
+				m_task->run();
+			}
+		}
+	};
+}
+
+#endif

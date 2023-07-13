da22adaa57f447aa9de99c3c38382840ce12de2d
Torbjörn Rathsman <milasudril@gmail.com>
2023-07-13 20:25:20 +0200

    Use message_decoder from create_task_server

--- middleware/create_task_server.hpp
+++ middleware/create_task_server.hpp
@@ -3,6 +3,7 @@
 
 #include "./http_request_result.hpp"
 #include "./task_registry.hpp"
+#include "./message_decoder.hpp"
 
 #include <west/http_message_header.hpp>
 #include <compare>
@@ -13,8 +14,6 @@ namespace restore
 	{
 	public:
 		explicit create_task_server(task_registry& tasks):
-			m_request_body{std::make_unique<jopp::container>()},
-			m_request_body_parser{*m_request_body},
 			m_resp_ptr{nullptr},
 			m_bytes_to_read{0},
 			m_tasks{tasks}
@@ -27,7 +26,7 @@ namespace restore
 		{
 			try
 			{
-				auto obj = m_request_body->get_if<jopp::object>();
+				auto obj = m_msg_decoder.get_json()->get_if<jopp::object>();
 				if(obj == nullptr)
 				{ throw std::runtime_error{"Expected request to be an object"}; }
 
@@ -69,27 +68,10 @@ namespace restore
 		}
 
 		auto process_request_content(std::span<char const> buffer)
-		{
-			auto const res = m_request_body_parser.parse(buffer);
-			auto const bytes_written = static_cast<size_t>(res.ptr - std::begin(buffer));
-
-			if(res.ec == jopp::parser_error_code::completed || res.ec == jopp::parser_error_code::more_data_needed)
-			{
-				return http_write_req_result{
-					.bytes_written = bytes_written,
-					.ec = http_req_processing_result{res.ec}
-				};
-			}
-
-			return http_write_req_result{
-				.bytes_written = bytes_written,
-				.ec = http_req_processing_result{res.ec}
-			};
-		}
+		{ return m_msg_decoder.process_request_content(buffer); }
 
 	private:
-		std::unique_ptr<jopp::container> m_request_body;
-		jopp::parser m_request_body_parser;
+		message_decoder m_msg_decoder;
 
 		std::string m_response;
 		char const* m_resp_ptr;
